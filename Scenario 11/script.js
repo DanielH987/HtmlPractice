df80dc0448f79c4b3a979f5889a251751a379f38
Daniel Hootini <hootinid@gmail.com>
2024-07-26 10:43:53 -1000

    Refactor color change function for better readability and maintainability

--- Scenario 11/script.js
+++ Scenario 11/script.js
@@ -1,10 +1,10 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    var title = document.getElementById('title');
-    var currentColor = title.style.color;
+function changeColor() {
+    let title = document.getElementById('title');
+    let currentColor = title.style.color;
 
     if (currentColor === 'blue') {
         title.style.color = 'red';
     } else {
         title.style.color = 'blue';
     }
-});
+}
