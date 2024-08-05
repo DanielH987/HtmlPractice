c3e5ce5d005a28c68175ce420252f0355026711b
Daniel Hootini <hootinid@gmail.com>
2024-08-05 11:01:35 -1000

    refactor: Add contact form submit event listener

---
+++ Scenario 17/script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    alert(`Thank you, ${name}! We will contact you at ${email}.`);
+});
