f96d1af29427980398785abb4358ff9f8d985d7b
Daniel Hootini <hootinid@gmail.com>
2024-08-28 14:19:36 -1000

    refactor: Update script.js for Scenario 18 page

--- Scenario 18/script.js
+++ Scenario 18/script.js
@@ -4,4 +4,10 @@ function submitQuiz() {
         q2: 'B',
         q3: 'A'
     };
+
+    let score = 0;
+    const form = document.getElementById('quiz-form');
+    const resultContainer = document.getElementById('result-container');
+    const results = document.getElementById('results');
+
 }
