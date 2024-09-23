047843aea5a78f7134b016fb6e9aad192aec8b3b
Daniel Hootini <hootinid@gmail.com>
2024-09-23 01:40:04 -0700

    Update script.js

--- Scenario 11/script.js
+++ Scenario 11/script.js
@@ -2,6 +2,7 @@ function changeColor() {
     let title = document.getElementById('title');
     let currentColor = title.style.color;
 
+    // Check if currentColor is blue
     if (currentColor === 'blue') {
         title.style.color = 'red';
     } else {
